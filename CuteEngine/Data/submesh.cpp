7ff3fbfd1eb72e50f5e2fd4ddec53db4be0af1be
HeladodePistacho <th_sola@hotmail.com>
2019-04-03 13:00:19 +0200

    Submesh Class prepared to Load

--- CuteEngine/Data/submesh.cpp
+++ CuteEngine/Data/submesh.cpp
@@ -1,6 +1,61 @@
 #include "submesh.h"
+#include <QOpenGLFunctions>
 
-Submesh::Submesh()
+Submesh::Submesh(VertexFormat format, void* _data, int size)
 {
+    vertex_format = format;
+    data = (unsigned char*)_data;
+    data_size = size * sizeof(float);
+}
+
+Submesh::Submesh(VertexFormat format, void* _data, int size, void* _indexs, int index_size)
+{
+    vertex_format = format;
+    data = (unsigned char*)_data;
+    data_size = size * sizeof(float);
+
+    indexs = (unsigned int*)_indexs;
+    indexs_count = index_size * sizeof(unsigned int);
+}
+
+void Submesh::ReLoad()
+{
+    QOpenGLFunctions* gl_functions = QOpenGLContext::currentContext()->functions();
+
+    vertex_buffer_object.create();
+    vertex_buffer_object.bind();
+    vertex_buffer_object.setUsagePattern(QOpenGLBuffer::UsagePattern::StaticDraw);
+    vertex_buffer_object.allocate(data, data_size);
+    delete data;
+    data = nullptr;
+
+    if(indexs_count > 0)
+    {
+        index_buffer_object.create();
+        index_buffer_object.bind();
+        index_buffer_object.setUsagePattern(QOpenGLBuffer::UsagePattern::StaticDraw);
+        index_buffer_object.allocate(indexs, indexs_count);
+        delete indexs;
+        indexs = nullptr;
+    }
+
+    vertex_array_object.create();
+    vertex_array_object.bind();
+    for(int i = 0; i < MAX_VERTEX_ATTRIBUTES; i++)
+    {
+        VertexAttribute& attribute = vertex_format.attribute[i];
+
+        if(attribute.enabled)
+        {
+            gl_functions->glEnableVertexAttribArray(GLuint(i));
+            gl_functions->glVertexAttribPointer(GLuint(i), attribute.num_components, GL_FLOAT, GL_FALSE, vertex_format.size, (void*)(attribute.offset));
+        }
+    }
+
+
+    vertex_array_object.release();
+    vertex_buffer_object.release();
+    if(index_buffer_object.isCreated())
+        index_buffer_object.release();
 
 }
--- CuteEngine/Data/submesh.h
+++ CuteEngine/Data/submesh.h
@@ -3,6 +3,7 @@
 
 #include <QOpenGLBuffer>
 #include <QOpenGLVertexArrayObject>
+#include <QOpenGLFunctions_3_3_Core>
 
 #define MAX_VERTEX_ATTRIBUTES 3
 
@@ -27,6 +28,14 @@ public:
         }
     }
 
+    VertexFormat& operator=(const VertexFormat& other_vertex_format)
+    {
+        memcpy(attribute, other_vertex_format.attribute, MAX_VERTEX_ATTRIBUTES);
+        size = other_vertex_format.size;
+
+        return *this;
+    }
+
     VertexAttribute attribute[MAX_VERTEX_ATTRIBUTES];
     int size = 0;
 };
@@ -34,11 +43,26 @@ public:
 class Submesh
 {
 public:
-    Submesh();
+    Submesh(VertexFormat format, void* data, int size);
+    Submesh(VertexFormat format, void* _data, int size, void* _indexs, int index_size);
+    ~Submesh();
+
+    void ReLoad();
+    void Draw();
+    void Destroy();
 
 private:
 
+    unsigned char* data = nullptr;
+    unsigned int data_size = 0;
 
+    unsigned int* indexs = nullptr;
+    unsigned int  indexs_count = 0;
+
+    VertexFormat vertex_format;
+    QOpenGLBuffer vertex_buffer_object;
+    QOpenGLBuffer index_buffer_object;
+    QOpenGLVertexArrayObject vertex_array_object;
 
 };
 
